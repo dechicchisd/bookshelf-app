90a57698fd4a13de852e25f9436795dd0de7442c
delfino <dechicchisd@gmail.com>
2022-05-07 22:30:32 +0200

    trying setting up psql

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ import render from 'koa-ejs'
 import { fileURLToPath } from 'url';
 import path from 'path';
 import serve from 'koa-static'
-import pg from 'pg'
 
-const { Client } = pg
-const client = new Client()
-await client.connect()
-await client.end()
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -24,6 +27,19 @@ render(app, {
 
 router.get('/', async ctx => {
     return ctx.render('index')
+})
+
+router.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 })
 
 app.use(router.routes())
